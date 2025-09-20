9331075bcec6382dd641c47b553f1d8919473605
Nghia Tran <vinhnghiatran@gmail.com>
2025-09-20 20:25:53 +0200

    Support SPM

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "WebPKit",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .watchOS(.v4),
+        .tvOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WebPKit",
+            targets: ["WebPKit"]
+        ),
+    ],
+    dependencies: [
+        // Provides the libwebp XCFrameworks for Apple platforms
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.3.0")
+    ],
+    targets: [
+        .target(
+            name: "WebPKit",
+            dependencies: [
+                .product(name: "libwebp", package: "libwebp-Xcode")
+            ],
+            path: "WebPKit",
+            exclude: [
+                "Supporting"
+            ]
+        )
+    ]
+)
+
+
